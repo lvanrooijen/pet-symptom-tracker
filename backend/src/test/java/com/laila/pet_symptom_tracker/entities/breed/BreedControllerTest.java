4c38275096b1793694a88417c341178f975d503b
Laila <175353500+lvanrooijen@users.noreply.github.com>
2025-03-25 17:18:35 +0100

    refactor BreedControllerTest

--- backend/src/test/java/com/laila/pet_symptom_tracker/entities/breed/BreedControllerTest.java
+++ backend/src/test/java/com/laila/pet_symptom_tracker/entities/breed/BreedControllerTest.java
@@ -1,23 +1,26 @@
 package com.laila.pet_symptom_tracker.entities.breed;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.laila.pet_symptom_tracker.entities.authentication.AuthenticationService;
 import com.laila.pet_symptom_tracker.entities.breed.dto.GetBreed;
+import com.laila.pet_symptom_tracker.entities.breed.dto.PatchBreed;
 import com.laila.pet_symptom_tracker.entities.breed.dto.PostBreed;
 import com.laila.pet_symptom_tracker.entities.pettype.dto.GetPetTypeCompact;
 import com.laila.pet_symptom_tracker.entities.user.UserService;
+import com.laila.pet_symptom_tracker.exceptions.generic.NotFoundException;
 import com.laila.pet_symptom_tracker.mainconfig.Routes;
 import com.laila.pet_symptom_tracker.securityconfig.JwtAuthFilter;
 import java.util.List;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -27,59 +30,140 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(controllers = BreedController.class)
 @AutoConfigureMockMvc(addFilters = false)
 class BreedControllerTest {
   @Autowired MockMvc mvc;
+  PostBreed postBreed;
+  GetBreed getBreed;
+  GetPetTypeCompact petType;
+  List<GetBreed> breeds;
+  Long breedId;
   @Autowired private ObjectMapper objectMapper;
-
   @MockitoBean private BreedService breedService;
   @MockitoBean private UserService userService;
   @MockitoBean private JwtAuthFilter jwtAuthFilter;
   @MockitoBean private Authentication authentication;
   @MockitoBean private AuthenticationService authenticationService;
 
-  @Test
-  @WithMockUser(username = "user")
-  void createBreed() throws Exception {
-    PostBreed postBreed = new PostBreed("Siamese", 1L);
-    GetBreed createdBreed = new GetBreed(1L, "Siamese", new GetPetTypeCompact(1L, "Cat"), "user");
-
-    when(breedService.create(any(PostBreed.class))).thenReturn(createdBreed);
-
-    mvc.perform(
-            post(Routes.BREEDS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(postBreed)))
-        .andExpect(status().isCreated())
-        .andExpect(header().exists("Location"))
-        .andExpect(jsonPath("$.id").value(1))
-        .andExpect(jsonPath("$.name").value("Siamese"));
-  }
-
-  @Test
-  void getAllBreeds() throws Exception {
-    List<GetBreed> breeds =
+  @BeforeEach
+  public void init() {
+    breedId = 1L;
+    postBreed = new PostBreed("Siamese", 1L);
+    getBreed =
+        new GetBreed(Long.valueOf(1), "Siamese", new GetPetTypeCompact(1L, "Cat"), "Moderator");
+    petType = new GetPetTypeCompact(1L, "Cat");
+    breeds =
         List.of(
             new GetBreed(1L, "Siamese", new GetPetTypeCompact(1L, "Cat"), "Moderator"),
             new GetBreed(2L, "Rotweiler", new GetPetTypeCompact(2L, "Dog"), "Moderator"),
             new GetBreed(3L, "Flemish Giant", new GetPetTypeCompact(3L, "Rabbit"), "Moderator"),
             new GetBreed(4L, "Parrot", new GetPetTypeCompact(4L, "Bird"), "ModOne"));
+  }
 
+  @Test
+  @WithMockUser(username = "user")
+  void createBreed() throws Exception {
+    given(breedService.create(ArgumentMatchers.any())).willReturn(getBreed);
+
+    ResultActions response =
+        mvc.perform(
+            post(Routes.BREEDS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(postBreed)));
+
+    response
+        .andExpect(MockMvcResultMatchers.status().isCreated())
+        .andExpect(
+            MockMvcResultMatchers.jsonPath("$.id", CoreMatchers.is(getBreed.id().intValue())))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.name", CoreMatchers.is(getBreed.name())))
+        .andExpect(
+            MockMvcResultMatchers.jsonPath(
+                "$.petType.name", CoreMatchers.is(getBreed.petType().name())))
+        .andExpect(
+            MockMvcResultMatchers.jsonPath(
+                "$.petType.id", CoreMatchers.is(getBreed.id().intValue())))
+        .andExpect(
+            MockMvcResultMatchers.jsonPath("$.createdBy", CoreMatchers.is(getBreed.createdBy())));
+  }
+
+  @Test
+  void getAllBreeds() throws Exception {
     when(breedService.getAll()).thenReturn(breeds);
-    ResultActions result =
-        mvc.perform(get(Routes.BREEDS))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.length()").value(breeds.size()));
 
-    for (int i = 0; i < breeds.size(); i++) {
-      result
-          .andExpect(jsonPath("$[" + i + "].id").value(breeds.get(i).id()))
-          .andExpect(jsonPath("$[" + i + "].name").value(breeds.get(i).name()))
-          .andExpect(jsonPath("$[" + i + "].petType.id").value(breeds.get(i).petType().id()))
-          .andExpect(jsonPath("$[" + i + "].petType.name").value(breeds.get(i).petType().name()))
-          .andExpect(jsonPath("$[" + i + "].createdBy").value(breeds.get(i).createdBy()));
-    }
+    ResultActions response =
+        mvc.perform(get(Routes.BREEDS).contentType(MediaType.APPLICATION_JSON));
+
+    response
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(breeds.size())));
+  }
+
+  @Test
+  void GetBreedById() throws Exception {
+    when(breedService.getById(1L)).thenReturn(getBreed);
+
+    mvc.perform(get(Routes.BREEDS + "/1"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(1))
+        .andExpect(jsonPath("$.name").value("Siamese"))
+        .andExpect(jsonPath("$.petType.id").value(1L))
+        .andExpect(jsonPath("$.petType.name").value("Cat"))
+        .andExpect(jsonPath("$.createdBy").value("Moderator"));
+  }
+
+  @Test
+  void GetNonExistentBreedById() throws Exception {
+    when(breedService.getById(breedId)).thenThrow(new NotFoundException());
+
+    mvc.perform(get(Routes.BREEDS + "/" + breedId.intValue())).andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void updateBreed() throws Exception {
+    PatchBreed patch = new PatchBreed("Changed", null);
+    GetBreed patchedBreed = new GetBreed(breedId, "Changed", petType, "Moderator");
+
+    when(breedService.patch(breedId, patch)).thenReturn(patchedBreed);
+
+    ResultActions response =
+        mvc.perform(
+            patch(Routes.BREEDS + "/" + breedId.intValue())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(patch)));
+
+    response
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(jsonPath("$.id").value(patchedBreed.id().intValue()))
+        .andExpect(jsonPath("$.name").value(patch.name()))
+        .andExpect(jsonPath("$.petType.id").value(patchedBreed.petType().id().intValue()))
+        .andExpect(jsonPath("$.petType.name").value(patchedBreed.petType().name()))
+        .andExpect(jsonPath("$.createdBy").value(patchedBreed.createdBy()));
+  }
+
+  @Test
+  public void deleteBreed() throws Exception {
+    doNothing().when(breedService).deleteById(breedId);
+
+    ResultActions response =
+        mvc.perform(
+            delete(Routes.BREEDS + "/" + breedId.intValue())
+                .contentType(MediaType.APPLICATION_JSON));
+
+    response.andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  public void deleteNonExistingBreed() throws Exception {
+    doThrow(new NotFoundException()).when(breedService).deleteById(breedId);
+
+    ResultActions response =
+        mvc.perform(
+            delete(Routes.BREEDS + "/" + breedId.intValue())
+                .contentType(MediaType.APPLICATION_JSON));
+
+    response.andExpect(MockMvcResultMatchers.status().isNotFound());
   }
 }
